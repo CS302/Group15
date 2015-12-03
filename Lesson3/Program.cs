6a0d7d218852fdf3a4c6383f883e2ac0009e6d81
alexMAI-302 <Alex71321@yandex.ru>
2015-12-03 22:05:54 +0400

    Lesson3
    Lesson3

---
+++ Lesson3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Test
+{
+    public static void Main()
+    {
+        int[] arr = { 6, 9, 3, 4, 1, 8, 7 };
+        int min = 100;
+        int minPos = 100;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (min > arr[i])
+            {
+                min = arr[i];
+                minPos = i;
+            }
+        }
+        Console.WriteLine("Minimum is " + min + " at position " + minPos);
+    }
+}
+
+
---
+++ Lesson3/Worker.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson3
+{
+    class Worker
+    {
+        private string name; //поле
+        private int age;     //поле
+        public int snn;     //поле
+        public static int count;
+        public int Age      //свойство
+        {
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Возраст задан неверно");
+                }
+                else
+                {
+                    age = value;
+                }
+            }
+            get { return age; }
+        }
+        public string Name
+        {
+            get { return name; }
+        }
+        
+        public void Print() //метод
+        {
+            Console.WriteLine("Имя: " + name);
+            Console.WriteLine("Возраст: " + age);
+            Console.WriteLine("ИНН: " + snn);
+            Console.WriteLine();
+        }
+
+        public static void PrintWorkers(Worker[] workers)
+        {
+            for (int i = 0; i < workers.GetLength(0); i++)
+            {
+                workers[i].Print();
+            }
+        }
+
+        public Worker(string name, int age, int snn) //параметризованный конструктор
+        {
+            this.name = name;
+            Age = age;
+            this.snn = snn;
+            count++;
+        }
+
+        public Worker(string name, int age)
+            : this(name, age, 0)
+        {   }
+
+        static Worker()
+        {
+            count = 0;
+        }
+    }
+}
